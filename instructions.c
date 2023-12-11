e883fcda7caf938750980063b9cf7aee97e35f1f
maelBuchs <mael@zzccmxtp>
2023-12-11 14:13:17 +0100

    add instruction.c

---
+++ instructions.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   instructions.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbuchs <mael@buchs.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 13:46:38 by mbuchs            #+#    #+#             */
+/*   Updated: 2023/12/11 13:46:38 by mbuchs           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	sa(t_data *data, int vers)
+/* switch a[0] et a[1] */
+{
+	if (!vers)
+		ft_putstr_fd("sa\n", 1);
+}
+
+void	sb(t_data *data, int vers)
+/* switch b[0] et b[1] */
+{
+	if (!vers)
+		ft_putstr_fd("sb\n", 1);
+}
+
+void	ss(t_data *data)
+/* sa + sb */
+{
+	sa(data, 1);
+	sb(data, 1);
+	ft_putstr_fd("ss\n", 1);
+}
+
+void	pa(t_data *data)
+/* b[0] -> a[0] */
+{
+	ft_putstr_fd("pa\n", 1);
+}
+
+void	pb(t_data *data)
+/* a[0] -> b[0] */
+{
+	ft_putstr_fd("pb\n", 1);
+}
+
+void	ra(t_data *data, int vers)
+/* a[last] -> a[0] || a[0] -> a[1] */
+{
+	if (!vers)
+		ft_putstr_fd("ra\n", 1);
+}
+
+void	rb(t_data *data, int vers)
+/* b[last] -> b[0] || b[0] -> b[1] */
+{
+	if (!vers)
+		ft_putstr_fd("rb\n", 1);
+}
+
+void	rr(t_data *data)
+/* ra + rb */
+{
+	ra(data, 1);
+	rb(data, 1);
+	ft_putstr_fd("rr\n", 1);
+}
+
+void	rra(t_data *data, int vers)
+/* a[0] -> a[last] || a[1] -> a[0] */
+{
+	if (!vers)
+		ft_putstr_fd("rra\n", 1);
+}
+
+void	rrb(t_data *data, int vers)
+/* b[0] -> b[last] || b[1] -> b[0] */
+{
+	if (!vers)
+		ft_putstr_fd("rrb\n", 1);
+}
+
+void	rrr(t_data *data)
+/* rra + rrb */
+{
+	rra(data, 1);
+	rrb(data, 1);
+	ft_putstr_fd("rrr\n", 1);
+}
--- push_swap.h
+++ push_swap.h
@@ -4,5 +4,20 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include "Libft/libft.h"
+
+typedef struct s_stack
+{
+	int		value;
+	t_stack	*next;
+	t_stack	*previous;
+}	t_stack;
+
+typedef struct s_data
+{
+	t_stack	*top_stack_a;
+	t_stack	*top_stack_b;
+	int		op_count;
+}	t_data;
 
 #endif
