6fb3993bc1570578dc00254b40cebd77d37da2f9
mbuchs <mael@buchs.fr>
2023-12-11 23:41:21 +0100

    d

--- main.c
+++ main.c
@@ -21,22 +21,26 @@ void	init_stack(t_stack *stack)
 
 void	add_node(t_stack *stack, long long value)
 {
-	t_node node;
+	t_node *node;
 
+	node = malloc(sizeof(t_node));
 	if(stack->range == 0)
 	{
-		node.next = &node;
-		node.previous = &node;
-		stack->top = &node;
-		stack->bottom = &node;
-		node.content = value;
+		node->next = node;
+		node->previous = stack->bottom;
+		stack->top = node;
+		stack->bottom = node;
+		node->content = value;
+		stack->bottom->next = node;
+		stack->range += 1;
 		return ;
 	}
-	node.next = stack->top->next;
-	node.previous = stack->bottom;
-	stack->top = &node;
-	node.next->previous = stack->top; 
-	node.content = value;
+	node->next = stack->top;
+	node->previous = stack->bottom;
+	stack->top = node;
+	node = stack->top;
+	node->content = value;
+	stack->bottom->next = stack->top;
 	stack->range += 1;
 }
 
@@ -45,8 +49,8 @@ void	print_stack(t_stack *stack)
 	int	i;
 
 	i = 0;
-	stack->selected = stack->top;
-	while (i < stack->range)
+	stack->selected = stack->bottom;
+	while (i < stack->range - 1)
 	{
 		stack->selected = stack->selected->next;
 		ft_putnbr_fd(stack->selected->content, 1);
@@ -58,8 +62,10 @@ void	print_stack(t_stack *stack)
 int main(void)
 {
 	t_stack stack_a;
+	t_stack stack_b;
 
 	init_stack(&stack_a);
+	add_node(&stack_a, 8);
 	add_node(&stack_a, 1);
 	add_node(&stack_a, 2);
 	add_node(&stack_a, 3);
@@ -68,7 +74,8 @@ int main(void)
 	add_node(&stack_a, 6);
 	add_node(&stack_a, 7);
 	add_node(&stack_a, 8);
+	init_stack(&stack_b);
 	print_stack(&stack_a);
-
+	print_stack(&stack_b);
 	return (0);
 }
